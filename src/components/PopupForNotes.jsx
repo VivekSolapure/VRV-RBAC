3c113a0d1c753928ce4242643c88046d911d7301
Vivek Solapure <solapurevivek2003@gmail.com>
2024-05-14 22:40:14 +0530

    Home page seperated with components, done with notes popup

---
+++ src/components/PopupForNotes.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+function PopupForNotes(props) {
+  const inputFileRef = useRef(null);
+  const popupRef = useRef(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  console.log(inputFileRef.value);
+  const triggerFileInputClick = () => {
+    if (inputFileRef.current) {
+      inputFileRef.current.click();
+    }
+  }
+
+  // const [fileName, setfileName] = useState(null)
+  // const handleFileChange = () => {
+  //   const files = inputFileRef.current.files;
+  //   console.log(files);
+  //   const names = [...files].map(file => file.name).join(", ");
+  //   setfileName(names)
+  // };
+  return (
+    <>
+          <textarea
+            name="newNoteTitle"
+            placeholder="Title"
+            id="newNoteTitle"
+          ></textarea>
+          <textarea
+            name="newNotePara"
+            placeholder="Type something here..."
+            id="newNotePara"
+          ></textarea>
+          {/* <div className="newNoteDropFile" onClick={triggerFileInputClick}>
+            <label htmlFor="input-file">
+              <div id="img-view">
+                <input type="file" ref={inputFileRef} onChange={handleFileChange} hidden multiple />
+                <img src="" alt="" />
+                <span>{fileName || 'DropFile'}</span>
+              </div>
+                <span>{fileName}</span>
+            </label>
+          </div> */}
+          <div className="home_saveBtn">Save</div>
+          <div className="home_deleteBtn">Delete</div>
+    </>
+  )
+}
+
+export default PopupForNotes
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,149 +1,21 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './Home.css';
+import NoteCard from '../components/NoteCard';
 
 function Home() {
 
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const popupRef = useRef(null);
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (!popupRef.current.contains(event.target)) setIsPopupOpen(false);
-    }
 
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-  const openPopup = () => {
-    setIsPopupOpen(true);
-  };
-
-  const inputFileRef = useRef(null);
-  console.log(inputFileRef.value);
-  const triggerFileInputClick = () => {
-    if (inputFileRef.current) {
-      inputFileRef.current.click();
-    }
-  }
-
-  const [fileName, setfileName] = useState(null)
-  const handleFileChange = () => {
-    const files = inputFileRef.current.files;
-    // Do something with the selected files
-    console.log(files);
-    const names = [...files].map(file => file.name).join(", ");
-    setfileName(names)
-  };
 
   return (
     <>
-      <div className={`home_popup ${isPopupOpen ? 'acticehome_popup' : ''}`}>
-        <section ref={popupRef} className="home_popupForNotes">
-          <textarea
-            name="newNoteTitle"
-            placeholder="Title"
-            id="newNoteTitle"
-          ></textarea>
-          <textarea
-            name="newNotePara"
-            placeholder="Type something here..."
-            id="newNotePara"
-          ></textarea>
-          <div className="newNoteDropFile" onClick={triggerFileInputClick}>
-            <label htmlFor="input-file">
-              <div id="img-view">
-                <input type="file" ref={inputFileRef} onChange={handleFileChange} hidden multiple />
-                <img src="" alt="" />
-                <span>{fileName || 'DropFile'}</span>
-              </div>
-                <span>{fileName}</span>
-            </label>
-          </div>
-        </section>
-      </div>
+      
       <div className="home_main">
         <div className="home_container">
           <div className="home_heading">
             <div className="home_headName">Notes Master</div>
             <div className="home_createBtn">Create</div>
           </div>
-          <div className="home_notesContainer">
-            <div className="home_notesList" onClick={openPopup}>
-              <div className="home_noteHeading">
-                <div className="home_noteName">Web Design</div>
-                <div className="home_noteEditLogo">
-                  <img src="./edit.svg" alt="" />
-                </div>
-              </div>
-              <div className="home_notePara">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt harum cupiditate officiis. Atque numquam sint nesciunt neque facilis accusamus. Fuga officiis sed alias reprehenderit deserunt repellendus suscipit veniam quidem voluptatum. From concept to launch, we create stunning, user-centric websites that elevate your brand and engage your audience.</div>
-            </div>
-            <div className="home_notesList">
-              <div className="home_noteHeading">
-                <div className="home_noteName">Web Design</div>
-                <div className="home_noteEditLogo">
-                  <img src="./edit.svg" alt="" />
-                </div>
-              </div>
-              <div className="home_notePara">From concept to launch, we create stunning, user-centric websites that elevate your brand and engage your audience.</div>
-            </div>
-            <div className="home_notesList">
-              <div className="home_noteHeading">
-                <div className="home_noteName">Web Design</div>
-                <div className="home_noteEditLogo">
-                  <img src="./edit.svg" alt="" />
-                </div>
-              </div>
-              <div className="home_notePara">From concept to launch, we create stunning, user-centric websites that elevate your brand and engage your audience.</div>
-            </div>
-            <div className="home_notesList">
-              <div className="home_noteHeading">
-                <div className="home_noteName">Web Design</div>
-                <div className="home_noteEditLogo">
-                  <img src="./edit.svg" alt="" />
-                </div>
-              </div>
-              <div className="home_notePara">From concept to launch, we create stunning, user-centric websites that elevate your brand and engage your audience.</div>
-            </div>
-            <div className="home_notesList">
-              <div className="home_noteHeading">
-                <div className="home_noteName">Web Design</div>
-                <div className="home_noteEditLogo">
-                  <img src="./edit.svg" alt="" />
-                </div>
-              </div>
-              <div className="home_notePara">From concept to launch, we create stunning, user-centric websites that elevate your brand and engage your audience.</div>
-            </div>
-            <div className="home_notesList">
-              <div className="home_noteHeading">
-                <div className="home_noteName">Web Design</div>
-                <div className="home_noteEditLogo">
-                  <img src="./edit.svg" alt="" />
-                </div>
-              </div>
-              <div className="home_notePara">From concept to launch, we create stunning, user-centric websites that elevate your brand and engage your audience.</div>
-            </div>
-            <div className="home_notesList">
-              <div className="home_noteHeading">
-                <div className="home_noteName">Web Design</div>
-                <div className="home_noteEditLogo">
-                  <img src="./edit.svg" alt="" />
-                </div>
-              </div>
-              <div className="home_notePara">From concept to launch, we create stunning, user-centric websites that elevate your brand and engage your audience.</div>
-            </div>
-            <div className="home_notesList">
-              <div className="home_noteHeading">
-                <div className="home_noteName">Web Design</div>
-                <div className="home_noteEditLogo">
-                  <img src="./edit.svg" alt="" />
-                </div>
-              </div>
-              <div className="home_notePara">From concept to launch, we create stunning, user-centric websites that elevate your brand and engage your audience.</div>
-            </div>
-
-          </div>
+          <NoteCard></NoteCard>
           <div className="home_seperation"><div className='home_sperationLine'></div></div>
           <div className="home_TodoContainer">
             <div className="home_Todoheading">TO DO</div>
